b9c6c558db08c36bf980bbaf2e32897a085850f0
aalis <andreylisogor@gmail.com>
2023-11-17 23:29:59 +0200

    stage 1

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import BlogPostView, BlogPostDetail
+from .views import (
+    BlogPostView,
+    BlogPostDetail,
+    BlogPostCreateView,
+    BlogPostUpdateView,
+    BlogPostDeleteView,
+)
 
 urlpatterns = [
+    path("post/delete/<int:pk>/", BlogPostDeleteView.as_view(), name="post_delete"),
+    path("post/edit/<int:pk>/", BlogPostUpdateView.as_view(), name="post_edit"),
+    path("post/new/", BlogPostCreateView.as_view(), name="post_new"),
     path("", BlogPostView.as_view(), name="home"),
     path("post/<int:pk>/", BlogPostDetail.as_view(), name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 
 
@@ -10,3 +11,21 @@ class BlogPostView(ListView):
 class BlogPostDetail(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+
+class BlogPostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["author", "title", "body"]
+
+
+class BlogPostUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+
+class BlogPostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = "/"
