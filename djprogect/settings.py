1c839091541092cec8ee08f6285d40bd39221d3e
aalis <andreylisogor@gmail.com>
2023-11-19 14:56:54 +0200

    forms and user registation

--- djprogect/settings.py
+++ djprogect/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
     "blog.apps.BlogConfig",
 ]
 
@@ -124,3 +125,6 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
